06db75a90d6b5133c72eed22082316fcab6bb94f
Quyu Kong <kongquyu@gmail.com>
2018-04-04 12:33:38 +1000

    Update start-demo.sh

--- start-demo.sh
+++ start-demo.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 python -m pip install --upgrade google-api-python-client
+sed -i "s/PYTHONEXEC <- \".*\"/PYTHONEXEC <- \"$(echo `which python` | sed 's/\//\\\//g')\"/g" app.R
 Rscript ./scripts/requiredPackages.R
 Rscript ./app.R &
